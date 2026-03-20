f97a2cd7090c33bb0992caccab0c098464e16a76
Baldragas <jhonurbina28@proton.me>
2026-03-19 22:37:45 -0400

    aprendiendo decoradores, modulacion del script

---
+++ Codigo principal/Cronograma2/prueba.py
@@ -0,0 +1,50 @@
+class Personaje:
+    def __init__(self, nombre, vida, fuerza):
+        self.nombre = nombre
+        # Usamos el nombre "público" para que pase por la aduana desde el inicio
+        self.vida = vida
+        self.fuerza = fuerza
+
+    # 1. LA ADUANA DE SALIDA (Getter)
+    # Sirve para leer el valor. Se usa el decorador @property
+    @property
+    def vida(self):
+        # Aquí devolvemos el valor real que está oculto en _vida
+        return self._vida
+
+    # 2. LA ADUANA DE ENTRADA (Setter)
+    # Aquí es donde pones las reglas de seguridad.
+    @vida.setter
+    def vida(self, valor_nuevo):
+        print(f"DEBUG: Intentando cambiar vida a {valor_nuevo}...")
+        
+        if valor_nuevo < 0:
+            print("  ¡Alarma! Intento de vida negativa. Ajustando a 0.")
+            self._vida = 0
+        elif valor_nuevo > 200:
+            self._vida = 200
+        else:
+            self._vida = valor_nuevo
+    @property
+    def fuerza(self):
+            return self._fuerza
+
+    @fuerza.setter
+    def fuerza(self, valor_nuevo):
+        print(f"DEBUG: Intentando cambiar fuerza a {valor_nuevo}...")
+        if valor_nuevo < 1:
+            print("La fuerza no puede ser menor a uno reajustando a valor por defecto: 1")
+            self._fuerza = 1
+        elif valor_nuevo > 100:
+            print("No se puede tener fuerza mayor a 100, reajustando a 100")
+            self._fuerza = 100
+        else:
+            self._fuerza = valor_nuevo
+# --- PRUEBA DE FUEGO ---
+
+heroe = Personaje("Arturo", 100, 250)
+
+# El usuario no sabe que hay una aduana, él solo escribe:
+heroe.vida = 200
+
+print(f"Resultado final: La vida de {heroe.nombre} es {heroe.vida}")
--- T.O.P/Script conversiones petroleras.py
+++ T.O.P/Script conversiones petroleras.py
@@ -1,43 +1,5 @@
-print("si no saludas, no eres amable")
-def libras_a_kg(libras):
-    resultado = libras * 0.453592
-    print(f"\n[RESULTADO]: {libras} lb equivalen a {resultado:.2f} kg")
+import Funciones_conversiones
 
-def ton_us_a_metrica(ton_us):
-    resultado = ton_us * 0.907185
-    print(f"\n[RESULTADO]: {ton_us} US Ton equivalen a {resultado:.2f} Toneladas Métricas.")
-
-def barriles_a_litros(bbl):
-    resultado = bbl * 158.98
-    print(f"\n[RESULTADO]: {bbl} bbl equivalen a {resultado:.2f} Litros.")
-
-def gal_a_litros(galones):
-    resultado = galones * 3.78541
-    print(f"\n[RESULTADO]: {galones} gal equivalen a {resultado:.2f} Litros.")
-
-def pies_a_metros(pies):
-    resultado = pies * 0.3048
-    print(f"\n[RESULTADO]: {pies} ft equivalen a {resultado:.2f} metros.")
-
-def pulgadas_a_mm(pulgadas):
-    resultado = pulgadas * 25.4
-    print(f"\n[RESULTADO]: {pulgadas} in equivalen a {resultado:.2f} milímetros.")
-
-def ppg_a_sg(ppg):
-    resultado = ppg / 8.33
-    print(f"\n[DENSIDAD]: {ppg} PPG equivalen a {resultado:.2f} SG (Gravedad Específica).")
-
-def sg_a_ppg(sg):
-    resultado = sg * 8.33
-    print(f"\n[DENSIDAD]: {sg} SG equivalen a {resultado:.2f} PPG.")
-
-def calcular_Ph(densidad, profundidad):
-    resultado = 0.052 * densidad * profundidad
-    print(f"\n[CÁLCULO TÉCNICO]:")
-    print(f"Con un lodo de {densidad} ppg a {profundidad} ft de profundidad,")
-    print(f"la Presión Hidrostática es de {resultado:.2f} PSI.")
-
-# --- INICIO DEL PROGRAMA ---
 print("--- Conversor Técnico de Unidades Petroleras ---")
 
 menu_principal = """
